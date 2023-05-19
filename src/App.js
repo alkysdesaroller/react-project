ecf304e236273b5f95da54acdb9ebbdf2ab25fd9
julio Cesar <julio_ccatb@hotmail.com>
2023-05-19 08:17:29 -0400

    fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import { CardComponent } from './components/Card';
 
 function App() {
+  const [pokemonid, setPokemonid] = useState(1);
+
   return (
     <div className='App'>
-      <CardComponent pokemonid={1} />
+      <CardComponent pokemonid={pokemonid} />
+      <button
+        onClick={() => {
+          setPokemonid(pokemonid + 1);
+        }}
+      >
+        Next
+      </button>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,4 @@
+'use client';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
@@ -6,20 +7,30 @@ export const CardComponent = ({ pokemonid }) => {
     name: '',
     types: [
       {
+        slot: 1,
         type: {
           name: 'grass',
           url: 'https://pokeapi.co/api/v2/type/12/',
         },
       },
     ],
-    sprites: [{ front_default: 'default' }],
+    sprites: { front_default: 'default' },
   });
 
   const getPokemon = async (pokemonid) => {
     const pokeresponse = await axios.get(
       `https://pokeapi.co/api/v2/pokemon/${pokemonid}`
     );
-    setPokemon(pokeresponse.data);
+
+    // if(!pokeresponse) return
+
+    const data = {
+      name: pokeresponse.data.name,
+      types: [...pokeresponse.data.types],
+      sprites: { front_default: pokeresponse.data.sprites.front_default },
+    };
+
+    setPokemon(data);
   };
 
   useEffect(() => {
@@ -28,10 +39,11 @@ export const CardComponent = ({ pokemonid }) => {
 
   return (
     <div className='card-bg'>
+      <img src={pokemon.sprites.front_default} />
       <h2>{pokemon.name}</h2>
       {pokemon.types.map((object) => {
         console.log(object);
-        return <p>{object.type}</p>;
+        return <p>{object.type.name}</p>;
       })}
     </div>
   );
